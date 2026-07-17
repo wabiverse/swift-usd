8b92c0e80815185e908f3c5b4858d76b38f844d9
Furby™ <furby@wabi.foundation>
2026-07-17 01:44:01 -0600

    Add missing include directory stubs to new Hio plugins.
    Signed-off-by: Furby™ <furby@wabi.foundation>

---
+++ Sources/HioImageIO/include/HioImageIO/_HioImageIO.h
@@ -0,0 +1,6 @@
+#ifndef __PXR_IMAGING_HIO_IMAGE_IO_H__
+#define __PXR_IMAGING_HIO_IMAGE_IO_H__
+
+// no public api headers.
+
+#endif // __PXR_IMAGING_HIO_IMAGE_IO_H__
---
+++ Sources/HioOpenEXR/include/HioOpenEXR/_HioOpenEXR.h
@@ -0,0 +1,6 @@
+#ifndef __PXR_IMAGING_HIO_OPENEXR_H__
+#define __PXR_IMAGING_HIO_OPENEXR_H__
+
+// no public api headers.
+
+#endif // __PXR_IMAGING_HIO_OPENEXR_H__
